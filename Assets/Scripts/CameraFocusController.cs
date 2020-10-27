f3ef99a984a8c6adb45403e42cc9dba8c19fbdab
Othmane Bakhtaoui <uo259323@uniovi.es>
2020-10-27 23:21:01 +0100

    now showing 3d objects

---
+++ Assets/Scripts/CameraFocusController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour {
+
+    private bool hasFuforiaStarted = false;
+
+    void Start () 
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+
+        if (vuforia != null)
+            vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+    }
+
+    private void StartAfterVuforia()
+    {
+        hasFuforiaStarted = true;
+        SetAutofocus();
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            if (hasFuforiaStarted)
+                SetAutofocus();
+        }
+    }
+
+    void SetAutofocus()
+    {
+        if (CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO))
+            Debug.Log("Autofocus set");
+        else
+            Debug.Log("No autofocus supported");
+    }
+}
---
+++ Assets/Scripts/VBInteraction.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBInteraction : MonoBehaviour
+{
+
+    VirtualButtonBehaviour[] virtualButtonBehaviours;
+    public GameObject firstPanel, secondPanel;
+    public GameObject cube, sphere, capsule, cylinder;
+    string vbName;
+
+    void Start()
+    {
+        //Register with the virtual buttons TrackableBehaviour
+        virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < virtualButtonBehaviours.Length; ++i)
+        {
+            virtualButtonBehaviours[i].RegisterOnButtonPressed(OnButtonPressed);
+            virtualButtonBehaviours[i].RegisterOnButtonReleased(OnButtonReleased);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        vbName = vb.VirtualButtonName;
+
+        if (vbName == "VirtualButtonChange")
+            VirtualButtonChange();
+        else if (firstPanel.activeInHierarchy)
+        {
+            ClearCanvas();
+            if (vbName == "ButtonV1")
+                ShowCube();
+            else if (vbName == "ButtonV2")
+                ShowSphere();
+        }
+        else if (secondPanel.activeInHierarchy)
+        {
+            ClearCanvas();
+            if (vbName == "ButtonV1")
+                ShowCapsule();
+            else if (vbName == "ButtonV2")
+                ShowCylinder();
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+            Debug.Log("ButtonReleased");
+    }
+
+    void VirtualButtonChange()
+    {
+        if (firstPanel.activeInHierarchy)
+        {
+            firstPanel.SetActive(false);
+            secondPanel.SetActive(true);
+        }
+        else if (secondPanel.activeInHierarchy)
+        {
+            firstPanel.SetActive(true);
+            secondPanel.SetActive(false);
+        }
+    }
+
+    void ClearCanvas()
+    {
+        cube.SetActive(false);
+        sphere.SetActive(false);
+        capsule.SetActive(false);
+        cylinder.SetActive(false);
+    }
+
+    void ShowCube()
+    {
+        Debug.Log("Cube");
+        cube.SetActive(true);
+    }
+
+    void ShowSphere()
+    {
+        Debug.Log("Sphere");
+        sphere.SetActive(true);
+    }
+
+    void ShowCapsule()
+    {
+        Debug.Log("Capsule");
+        capsule.SetActive(true);
+    }
+
+    void ShowCylinder()
+    {
+        Debug.Log("Cylinder");
+        cylinder.SetActive(true);
+    }
+
+}
